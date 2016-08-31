d00b8d177788dda8a19c7c5696a68148261f4d6a
THANAP <thana4622@gmail.com>
2016-09-01 05:43:37 +0700

    example.sh

---
+++ example.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+###############################################################################
+#  
+#              การติดตั้งระบบจัดการเว็บไซต์ DirectAdmin Control Panel             
+# 
+#          การติดตั้งที่ง่าย และไม่ยุ่งยาก เพียงไม่กี่ขั้นตอน พร้อมทั้งคำอธิบาย ภาษาไทย
+#                             
+#
+#               โดย Hosting Virgin [ www.hostingvirgin.com ]
+#			 มีปัญหาติดตั้งไม่ได้ ติดต่อ  support@hostingvirgin.com
+#
+###############################################################################
+
+#############   Hosting Virgin [ www.hostingvirgin.com ] #######################
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+sleep 4;
+wget -q -O da1443-en.tar.gz https://github.com/thanaphat/DA-Install/raw/master/da1443-en.tar.gz
+#################################################################################
+#################################################################################
+sleep 3;
+chmod 0755 da1443-en.tar.gz
+ls
+echo "";
+echo "";
+echo "";
+echo "";
+echo "DOWNLOAD FILE da1443-en.tar.gz COMPLETE";
+echo "";
+echo "";
+sleep 5;
+
